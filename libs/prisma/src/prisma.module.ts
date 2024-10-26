9187f02a23a45c3450b4a3efcbd70cc9363757f4
Supan Adit Pratama <supanadit@gmail.com>
2024-10-27 00:50:15 +0700

    feat: simplify the providers

--- libs/prisma/src/prisma.module.ts
+++ libs/prisma/src/prisma.module.ts
@@ -1,5 +1,12 @@
+import { READ_ENTIRE_PROCESS } from '@autocrud/skeleton';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaListProcess } from './processes/list.process';
+
+export const configServiceProvider = {
+  provide: READ_ENTIRE_PROCESS,
+  useClass: PrismaListProcess,
+};
 
 @Module({
   providers: [PrismaService],
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { PrismaModule, PrismaService } from '@autocrud/prisma';
+import {
+  configServiceProvider,
+  PrismaModule,
+  PrismaService,
+} from '@autocrud/prisma';
 import { PRISMA_DELEGATE } from '@autocrud/prisma/constants';
-import { ListProcess } from '@autocrud/skeleton/processes/list.process';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { READ_ENTIRE_PROCESS } from '@autocrud/skeleton';
-import { PrismaListProcess } from '@autocrud/prisma/processes/list.process';
 
 const customFactory = {
   provide: PRISMA_DELEGATE,
@@ -16,13 +17,8 @@ const customFactory = {
 };
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    PrismaService,
-    customFactory,
-    { provide: READ_ENTIRE_PROCESS, useClass: PrismaListProcess },
-  ],
+  providers: [AppService, customFactory, configServiceProvider],
 })
 export class AppModule {}
