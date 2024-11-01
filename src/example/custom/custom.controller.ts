bb23bd0db99135f947bf801990aeedbed1066db2
Supan Adit Pratama <account@supanadit.com>
2024-11-02 01:15:35 +0700

    chore: sample override

--- src/example/custom/custom.controller.ts
+++ src/example/custom/custom.controller.ts
@@ -1,7 +1,13 @@
 import { CustomCRUDController } from '@aditama-labs/nest-autocrud/skeleton';
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('example/custom')
 export class CustomController extends CustomCRUDController({
   uniqueIdentifier: 'username',
-}) {}
+}) {
+  @Get('list')
+  async list(): Promise<any> {
+    console.log('Hello World');
+    return await super.list();
+  }
+}
