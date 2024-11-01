50d02ea9ffdf3c6eb1c003dd6f0a501046a4dea2
Supan Adit Pratama <account@supanadit.com>
2024-11-02 01:07:19 +0700

    wip: separate some controller interface to be fine grain later

--- libs/skeleton/src/interfaces/controller/skeleton-crud.controller.interface.ts
+++ libs/skeleton/src/interfaces/controller/skeleton-crud.controller.interface.ts
@@ -1,10 +1,23 @@
 import { IPaginationParam } from '../pagination-param.interface';
 
-export interface ISkeletonCRUDController {
-  create(body);
-  delete(id);
+export interface ISkeletonReadController {
   list();
-  pagination(params: IPaginationParam);
   read(id);
+  pagination(params: IPaginationParam);
+}
+
+export interface ISkeletonDeleteController {
+  delete(id);
+}
+export interface ISkeletonCreateController {
+  create(body);
+}
+export interface ISkeletonUpdateController {
   update(id, body);
 }
+
+export interface ISkeletonCRUDController
+  extends ISkeletonCreateController,
+    ISkeletonReadController,
+    ISkeletonDeleteController,
+    ISkeletonUpdateController {}
