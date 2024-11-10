691a00293c6fa91401670397eec16e8d1451cf33
Supan Adit Pratama <supanadit@gmail.com>
2024-11-10 12:25:58 +0700

    chore: fix not read the .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { PrismaCustomModule } from './example/prisma/custom/custom.module';
 import { PrismaSimpleModule } from './example/prisma/simple/simple.module';
 import { TypeORMSimpleModule } from './example/typeorm/simple/simple.module';
 import { TypeORMCustomModule } from './example/typeorm/custom/custom.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PrismaCustomModule,
     PrismaSimpleModule,
     TypeORMSimpleModule,
